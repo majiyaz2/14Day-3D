5dcaea4503743583c5a3333d8ec19cbfbdb50649
Andile Mhlongo <majiyaz2@gmail.com>
2025-11-17 21:53:17 +0200

    feat: added a brightness and contrast configuration

--- postprocessing/src/components/Effects.jsx
+++ postprocessing/src/components/Effects.jsx
@@ -2,6 +2,7 @@ import { EffectComposer } from "@react-three/postprocessing";
 import { Vignette } from "@react-three/postprocessing";
 import { useControls } from "leva"; 
 import { Bloom } from "@react-three/postprocessing";
+import { BrightnessContrast } from "@react-three/postprocessing";
 
 export const Effects = () => {
   const vignetteConfig = useControls("vignette", {
@@ -17,10 +18,18 @@ export const Effects = () => {
     intensity: { value: 1.28, min: 0, max: 2 },
   });
 
+  const brightnessContrastConfig = useControls("brightnessContrast", {
+    enabled: true,
+    brightness: { value: 0.02, min: -1, max: 1 },
+    contrast: { value: -0.1, min: -1, max: 1 },
+  });
+
   return (
     <EffectComposer disableNormalPass>
       {vignetteConfig.enabled && <Vignette {...vignetteConfig} />}
       {bloomConfig.enabled && <Bloom {...bloomConfig} />}
+      {brightnessContrastConfig.enabled && <BrightnessContrast {...brightnessContrastConfig} />}
+
     </EffectComposer>
   );
 };
