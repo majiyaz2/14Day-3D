ff70a9a5919cd1f8bffdbd435d76bdc9faccf558
Andile Mhlongo <majiyaz2@gmail.com>
2025-11-17 21:46:34 +0200

    feat: added a bloom configuration

--- postprocessing/src/components/Effects.jsx
+++ postprocessing/src/components/Effects.jsx
@@ -1,6 +1,7 @@
 import { EffectComposer } from "@react-three/postprocessing";
 import { Vignette } from "@react-three/postprocessing";
-import { useControls } from "leva";
+import { useControls } from "leva"; 
+import { Bloom } from "@react-three/postprocessing";
 
 export const Effects = () => {
   const vignetteConfig = useControls("vignette", {
@@ -9,9 +10,17 @@ export const Effects = () => {
     darkness: { value: 0.92, min: 0, max: 1 },
   });
 
+  const bloomConfig = useControls("bloom", {
+    enabled: true,
+    mipmapBlur: true,
+    luminanceThreshold: { value: 1, min: 0, max: 2 },
+    intensity: { value: 1.28, min: 0, max: 2 },
+  });
+
   return (
     <EffectComposer disableNormalPass>
       {vignetteConfig.enabled && <Vignette {...vignetteConfig} />}
+      {bloomConfig.enabled && <Bloom {...bloomConfig} />}
     </EffectComposer>
   );
 };
