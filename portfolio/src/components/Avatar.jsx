4716ee0325924a906186fb3cbeabdbbeefcf3a50
Andile Mhlongo <majiyaz2@gmail.com>
2025-12-05 22:07:04 +0200

    feat: added animations

--- portfolio/src/components/Avatar.jsx
+++ portfolio/src/components/Avatar.jsx
@@ -1,10 +1,32 @@
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import React, { useEffect, useRef, useState } from 'react'
+import { useAnimations, useFBX, useGLTF } from '@react-three/drei'
 
 export function Avatar(props) {
   const { nodes, materials } = useGLTF('/models/693143bd76dccadf3d05ea03.glb')
+const { animations: idleAnimation } = useFBX("/animations/Idle.fbx");
+  const { animations: walkingAnimation } = useFBX("/animations/Walking.fbx");
+  const { animations: runningAnimation } = useFBX("/animations/Running.fbx");
+
+  idleAnimation[0].name = 'Idle'
+  walkingAnimation[0].name = 'Walking'
+  runningAnimation[0].name = 'Running'
+
+  const group = useRef()
+  const {actions} = useAnimations(
+    [idleAnimation[0], walkingAnimation[0], runningAnimation[0]],
+    group
+  )
+
+  const [animation, setAnimation] = useState('Running')
+
+  useEffect(() => {
+    actions[animation].reset().fadeIn(0.5).play()
+    return () => {
+      actions[animation].fadeOut(0.5)
+    }
+  }, [animation])
   return (
-    <group {...props} dispose={null}>
+    <group {...props} dispose={null} ref={group}>
       <primitive object={nodes.Hips} />
       <skinnedMesh
         name="EyeLeft"
@@ -67,4 +89,7 @@ export function Avatar(props) {
   )
 }
 
+useFBX.preload('/animations/Running.fbx')
+useFBX.preload('/animations/Idle.fbx')
+useFBX.preload('/animations/Walking.fbx')
 useGLTF.preload('/models/693143bd76dccadf3d05ea03.glb')
