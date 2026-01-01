c6138d7ce3887ed33c9d57fa30927d209cb3ec09
Andile Mhlongo <majiyaz2@gmail.com>
2026-01-01 18:22:43 +0200

    feat: added a projects html view

--- portfolio/src/config.js
+++ portfolio/src/config.js
@@ -23,6 +23,33 @@ export const config = {
             icon: "icons/threejs.png",
             level: 70,
         },
-    ]
+    ],
+    projects: [
+    {
+      name: "Pictometer",
+      description: "A generative AI image creation studio with editing capabilities",
+      image: "projects/project1.jpg",
+      link: "https://www.youtube.com/watch?v=ma9t7HAOZRg",
+    },
+    {
+      name: "Blog Spot",
+      description: "A blogging application where users can create and view posts",
+      image: "projects/project2.jpg",
+      link: "https://www.youtube.com/watch?v=SOF7GBmC6gE",
+    },
+    {
+      name: "Farm2Table",
+      description: "A multi-tenant e-commerce platform for farmers to sell their fruit and vegetables",
+      image: "projects/project3.jpg",
+      link: "https://www.youtube.com/watch?v=2W_VR92Pqgs",
+    },
+    {
+      name: "Neurodrive",
+      description: "A self driving car platform where the neural engine is ran by an AI",
+      image: "projects/project4.jpg",
+      link: "https://www.youtube.com/watch?v=pGMKIyALcK0",
+    },
+  ],
+
 
 }
